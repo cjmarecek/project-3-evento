6b941f07401f470705fca51252fcb237d52246e0
cjmarecek <cj.marecek@gmail.com>
2020-05-26 17:08:54 +0200

    anonymous auth working, upgrading to expo SDK37

--- App.js
+++ App.js
@@ -1,32 +1,68 @@
-import React from 'react';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useState, useEffect } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+// navigation
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import MainDrawer from "./navigation/MainDrawer";
 // import { MenuProvider } from 'react-native-popup-menu';
 // import HeaderDropdown from './components/HeaderDropdown';
 
-import { store, persistor } from './redux/store';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// redux
+import { store, persistor } from "./redux/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { Stitch, AnonymousCredential } from 'mongodb-stitch-react-native-sdk';
-
-import MainDrawer from './navigation/MainDrawer';
-
-const stitchAppId = "keric-app-fouoc";
+// stitch
+import { Stitch, AnonymousCredential } from "mongodb-stitch-react-native-sdk";
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      {/* <MenuProvider customStyles={menuProviderStyles}> */}
-      <PersistGate loading={null} persistor={persistor}>
-        <NavigationContainer>
-          <MainDrawer />
-        </NavigationContainer>
-      </PersistGate>
-      {/* </MenuProvider> */}
-    </Provider>
-  );
-}
+
+  const [currentUserId, setCurrentUserId] = useState({});
+  const [client, setClient]  = useState({});
+
+  useEffect(() => {
+    _loadClient();
+  },[]);
+
+  const _loadClient = () => {
+    Stitch.initializeDefaultAppClient("keric-app-fouoc").then((clientParam) => {
+      setClient({ client: clientParam });
+
+      if (clientParam.auth.isLoggedIn) {
+        setCurrentUserId({ setCurrentUserId: clientParam.auth.user.id });
+      } else {
+        client.auth.loginWithCredential(new AnonymousCredential()).then(user => {
+          console.log(`Successfully logged in as user ${user.id}`);
+          setCurrentUserId({  currentUserId: user.id })
+      }).catch(err => {
+          console.log(`Failed to log in anonymously: ${err}`);
+          setCurrentUserId({  currentUserId: null })
+      });
+      }
+    });
+  }
+    return (
+        <Provider store={store}>
+          {/* <MenuProvider customStyles={menuProviderStyles}> */}
+          <PersistGate loading={null} persistor={persistor}>
+            <NavigationContainer>
+              <MainDrawer />
+            </NavigationContainer>
+          </PersistGate>
+          {/* </MenuProvider> */}
+        </Provider>
+    );
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 // const styles = StyleSheet.create({
 //   container: {
